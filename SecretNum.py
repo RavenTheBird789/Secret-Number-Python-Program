164954040c2d177a9f0973cffa2d07f39d7269f5
Art3mis_XP <northwalker676@gmail.com>
2025-11-26 03:55:07 +0100

    Update SecretNum.py

--- SecretNum.py
+++ SecretNum.py
@@ -4,24 +4,12 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = int(input(f"Guess a number between 1 and {x}: "
+        guess = int(input(f"Guess a number between 1 and {x}: "))
         if guess < random_number:
             print("Sorry, guess again. Too low.")
         elif guess > random_number: 
             print("Sorry, guess again. Too high.") 
 
     print(f"Congrats! You have guessed the number {random_number} correctly!")
-    
-    user_request = input("Would you like to play again? (yes/no): ")
-    if user_request == "yes":
-        guess();
-    elif user_request == "y":
-        guess()
-    elif user_request == "Yes":
-        guess();
-    elif user_request == "Y":
-        guess();
-    else:
-        print("Have a nice day!");
-        exit();
-guess(100);
+
+guess(10)
